6dea4e68e01361991546a4dcb2288da6bd91dc70
Deku-shrub <chrismmonteiro1+github@gmail.com>
2016-08-28 12:23:57 +0100

    boring sidebar refactor

---
+++ sidebar.js
@@ -0,0 +1,7 @@
+document.getElementById("sidebar").innerHTML =
+ '<p><a href="/">Home</a></p>'+
+  '<p><a href="/Scripture/Proof_Google_Is_God.html">Proof</a></p>'+
+  '<p><a href="/faq.html">F.A.Q.</a></p>'+
+  '<p><a href="/Scripture/google_prayers.html">Prayers</a></p>'+
+  '<p><a href="/Scripture/10_Commandments.html">Comms</a></p>'+
+  '<p><a href="/contact.html">Contact</a></p>';
