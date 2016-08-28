9a1993b791d7c1905d037b43dcf73489231ab2e8
Deku-shrub <chrismmonteiro1+github@gmail.com>
2016-08-28 13:43:15 +0100

    footer

--- footer.js
+++ footer.js
@@ -1,2 +1,2 @@
 document.getElementById("footer").innerHTML =
- '<p align="center" class="footer">Created by <a href="https://www.facebook.com/mattmacpherson">Matt MacPherson</a></p>';
+ '<p align="center" class="footer">Founded by <a href="/contact.html">Matt MacPherson</a></p>';
