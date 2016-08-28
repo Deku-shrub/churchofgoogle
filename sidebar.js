def9db504984e5d1ecefa2c58cf6ca5a8d82dacb
Deku-shrub <chrismmonteiro1+github@gmail.com>
2016-08-28 13:38:56 +0100

    hate mail integrated plus flattening structure

--- sidebar.js
+++ sidebar.js
@@ -1,10 +1,11 @@
 document.getElementById("sidebar").innerHTML =
  '<p><a href="/">Home</a></p>'+
-  '<p><a href="/Scripture/Proof_Google_Is_God.html">Proof</a></p>'+
+  '<p><a href="/Proof_Google_Is_God.html">Proof</a></p>'+
   '<p><a href="/faq.html">F.A.Q.</a></p>'+
-  '<p><a href="/Scripture/google_prayers.html">Prayers</a></p>'+
-  '<p><a href="/Scripture/10_Commandments.html">Commandments</a></p>'+
+  '<p><a href="/google_prayers.html">Prayers</a></p>'+
+  '<p><a href="/10_Commandments.html">Commandments</a></p>'+
   '<p><a href="/search-tips.html">Search Tips</a></p>'+
-  '<p><a href="/Scripture/google_appreciation_day.html">Appreciation Day</a></p>'+
-  '<p><a href="/Scripture/promotional_campaign.html">Promote</a></p>'+
+  '<p><a href="/google_appreciation_day.html">Appreciation Day</a></p>'+
+  '<p><a href="/promotional_campaign.html">Promote</a></p>'+
+  '<p><a href="/hate_mail.html">Promote</a></p>'+  
   '<p><a href="/contact.html">Contact</a></p>';
