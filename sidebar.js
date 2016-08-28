13da93bae75ccce5012be14b243382c56bef94ea
Deku-shrub <chrismmonteiro1+github@gmail.com>
2016-08-28 12:59:40 +0100

    moved search tips

--- sidebar.js
+++ sidebar.js
@@ -5,4 +5,4 @@ document.getElementById("sidebar").innerHTML =
   '<p><a href="/Scripture/google_prayers.html">Prayers</a></p>'+
   '<p><a href="/Scripture/10_Commandments.html">Comms</a></p>'+
   '<p><a href="/contact.html">Contact</a></p>'+
-  '<p><a href="/search-tips/index.html">Search Tips</a></p>';
+  '<p><a href="/search-tips.html">Search Tips</a></p>';
